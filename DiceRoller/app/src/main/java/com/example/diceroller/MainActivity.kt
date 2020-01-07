e5281da499b44c87c79d149c0f2261331f65cf42
kyledunne <kyledunne@berkeley.edu>
2020-01-07 01:25:55 -0500

    Complete Lesson 1: Dice Roller

---
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+    val RAND = Random()
+    lateinit var dieImage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        dieImage = findViewById(R.id.die_image)
+
+        rollButton.setOnClickListener {
+            val rollResult = RAND.nextInt(6) + 1
+            dieImage.setImageResource(when (rollResult) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            })
+        }
+    }
+}
